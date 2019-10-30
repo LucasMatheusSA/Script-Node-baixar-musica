97dbb89658806b1ae52cc30e57a5e32800a03e10
Lucas <lucas.matsalves@gmail.com>
2019-10-30 16:54:51 -0300

    Checagem de video e site conversor de audio

--- index.js
+++ index.js
@@ -6,67 +6,86 @@ const file = "Songs.txt"
 
 LINK_YOUTUBE = 'https://www.youtube.com';
 
+searchVideo();
+
 function getSongs(){
     let data = fs.readFileSync(file,'utf8');
     data = data.split("\r");
-    return data;
+    if(data == ''){
+        return null;        
+    }else{
+        return data;
+    }
 }
 
-function corectString(item){
-    item = item.replace("\r","");
-    item = item.replace(" ","+");
+function convertWord(item){
+    item = item.trim();
+    item = item.replace(/\r/g,"");
+    item = item.replace(/\n/g,"");
+    item = item.replace(/ /g,"+");
+    return item;
 }
 
 function searchVideo(){
     let songs = getSongs();
+    let links = [];
 
-    songs.forEach(function (item,index){
-        item = item.replace(/\r/g,"");
-        item = item.replace(/\n/g,"");
-        item = item.replace(/ /g,"+");
-        
-        request(LINK_YOUTUBE + "/results?search_query=" + item + "&pbj=1", function (erro,response,html){
-            if(!erro && response.statusCode == 200){
-            const telaPesquisa = cheerio.load(html);
-            telaPesquisa('a').each((i,el)=>{
-                const htmlVideo = telaPesquisa(el).attr('href');
-                if(htmlVideo.indexOf('watch') != -1){
-                    downloadSong(htmlVideo,item);
-                }
-                })
-            }else{
-                console.log("Deu ruim na pesquisa pelo nome (" + item + ")!!!");
-            }
-        })
-    })
-}
+    if(songs != null){
 
-function downloadSong(htmlVideo,texto){
-    console.log("entrou");
-    request(LINK_YOUTUBE + htmlVideo + "&pbj=1", function (erro,response,html){
-        if(!erro && response.statusCode == 200){
-            const telaVideo = cheerio.load(html);
-            telaVideo('yt-formatted-string').each((i,el)=>{
-                console.log("entrou1");
-                const classTitle = telaVideo(el).attr('class');
-                if(classTitle == 'style-scope ytd-video-primary-info-renderer'){
-                    if(true){
-                        console.log("aqqqqq");
-                        console.log(telaVideo(el).text());
-                    }else{
-                        console.log("Deu ruim compatibilidade de titulo (" + texto + ")!!!");
-                    }
+        songs.forEach(function (item,index){
+            item = convertWord(item);
+            
+            request(LINK_YOUTUBE + "/results?search_query=" + item + "&pbj=1", function (erro,response,html){
+                if(!erro && response.statusCode == 200){
+                    const telaPesquisa = cheerio.load(html);    
+                    
+                    telaPesquisa('a').each((i,el)=>{
+                        const htmlVideo = telaPesquisa(el).attr('href');
+                        
+                        if(htmlVideo.indexOf('watch') != -1){
+                            links.push(htmlVideo);
+                        }
+                    })
+
+                    checkSong(links[0],item);
+
                 }else{
-                    console.log("Deu ruim na pesquisa pelo titulo (" + texto + ")!!!");
+                    console.log("Deu ruim na pesquisa pelo nome (" + item + ")!!!");
                 }
             })
+        })
+    }else{
+        console.log("Não tem nenhuma musica !!!");
+    }
+}
+    
+function checkSong(htmlVideo,texto){
+        request(LINK_YOUTUBE + htmlVideo + "&pbj=1", function (erro,response,html){
+            if(!erro && response.statusCode == 200){
+            const telaVideo = cheerio.load(html);
+
+            const title = convertWord(telaVideo('.watch-title').html());
+
+            if(texto == title){
+                let url = LINK_YOUTUBE + htmlVideo;
+                url = url.replace("youtube","yout");
+                downloadSong(url);
+            }else{
+                console.log("Deu ruim, video errado ... ("+ texto +")");
+            }
         }else{
             console.log("Deu ruim na tela de videos !!!");
         }
     })
 }
 
-searchVideo();
+function downloadSong(url){
+    request(url, function(erro,response,html){
+        if(!erro && response.statusCode == 200){
+            
+        }
+    })
+}
 
 
 
